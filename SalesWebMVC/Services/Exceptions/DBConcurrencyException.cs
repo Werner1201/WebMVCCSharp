e0931471dc5f1bb6fc0e08d989ccd97a43e2c3c0
Werner1201 <werner.romling@unigranrio.br>
2020-03-09 09:13:24 -0300

    Update seller and custom service exception

---
+++ SalesWebMVC/Services/Exceptions/DBConcurrencyException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMVC.Services.Exceptions
+{
+    public class DBConcurrencyException : ApplicationException
+    {
+        public DBConcurrencyException(string message) : base(message)
+        {
+
+        }
+    }
+}
